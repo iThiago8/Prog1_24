b0c2c404da7ab1e01b0ef42b1c10ce8d7474aa5c
Thiago Pedro Padilha <411730@unoesc.lan>
2024-03-04 21:59:17 -0300

    Estruturando a calculadora

---
+++ Atividades Aula/Atividade_240304/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+WriteLine("Olá, esse é um projeto de calculadora utilizando C#. Para começarmos, digite o primeiro valor:");
+double num1 = Convert.ToDouble(ReadLine());
+
+WriteLine("Agora digite qual operação você quer fazer (+, -, * ou /)");
+string? operador = ReadLine();
+
+WriteLine("Agora digite o segundo valor:");
+double num2 = Convert.ToDouble(ReadLine());
+
+double res = num1 + num2;
