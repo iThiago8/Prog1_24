8673c656d7423aa4df880a57e214250fbf864b7f
Thiago Pedro Padilha <411730@unoesc.lan>
2024-03-04 21:06:53 -0300

    Aula 03

--- Program.cs
+++ Program.cs
@@ -122,3 +122,17 @@ bool isNomeEqual2 =
     string.Equals(nomeCompleto, "Thiago Pereira Padilha");
 
 WriteLine($"Primeiro resultado = {isNomeEqual}, Segundo resultado = {isNomeEqual2}");
+
+//Tipos numéricos
+/*
+    sbyte: armazena valores entre -128 e 127;
+    short: armazena valores entre -32,768 e 32,767;
+    int: armazena valores entre -2,147,483,648 e 2,147,483,647
+    long: armazena valores entre -9,223,372,036,854,775,808 e 9,223,372,036,854,775,807
+*/
+
+float myFloat = 10f;
+double myDouble = 5d;
+decimal myDecimal = 7m;
+
+WriteLine($"{myFloat}, {myDouble}, {myDecimal}");
