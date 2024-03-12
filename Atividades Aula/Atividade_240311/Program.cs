0298e2e1a40440a726df1405dfc5a3a3770df2c8
iThiago8 <thiago.pedro.padilha899@gmail.com>
2024-03-12 18:43:01 -0300

    Separação de classe e método

--- Atividades Aula/Atividade_240311/Program.cs
+++ Atividades Aula/Atividade_240311/Program.cs
@@ -1,65 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Escreva um programa C# que seja capaz de pergutar ao usuário um operador aritmético específico e com base na operação escolhida pelo usuário, imprima a tabuada de 1 a 9 daquela operação. Utiliza uma formatação de impressão de laço de repetição for ou while para exibir a tabuada completa dinamicamente;
+﻿//Escreva um programa C# que seja capaz de pergutar ao usuário um operador aritmético específico e com base na operação escolhida pelo usuário, imprima a tabuada de 1 a 9 daquela operação. Utiliza uma formatação de impressão de laço de repetição for ou while para exibir a tabuada completa dinamicamente;
 
 
-bool loop = true;
-while(loop)
+internal class Program
 {
-    WriteLine("Digite qual operação você quer fazer (+, -, * ou /) ou (0) para encerrar.");
-
-    string? operador = ReadLine();
-
-    switch (operador)
+    private static void Main(string[] args)
     {
-        case "+":
-            for (float p2 = 1; p2 <= 9; p2++)
+        bool loop = true;
+        while (loop)
+        {
+            WriteLine("Digite qual operação você quer fazer (+, -, * ou /) ou (0) para encerrar.");
+
+            string? operador = ReadLine();
+
+            switch (operador)
             {
-                for (float p1 = 1; p1 <= 9; p1++)
-                {
-                    WriteLine($"{p1} + {p2} = {p1 + p2}");
-                }
-                WriteLine("--------------------");
+                case "+":
+                    for (float p2 = 1; p2 <= 9; p2++)
+                    {
+                        for (float p1 = 1; p1 <= 9; p1++)
+                        {
+                            WriteLine($"{p1} + {p2} = {p1 + p2}");
+                        }
+                        WriteLine("--------------------");
+                    }
+                    loop = false;
+                    break;
+                case "-":
+                    for (float p2 = 1; p2 <= 9; p2++)
+                    {
+                        for (float p1 = 1; p1 <= 9; p1++)
+                        {
+                            WriteLine($"{p1} - {p2} = {p1 - p2}");
+                        }
+                        WriteLine("--------------------");
+                    }
+                    loop = false;
+                    break;
+                case "*":
+                    for (float p2 = 1; p2 <= 9; p2++)
+                    {
+                        for (float p1 = 1; p1 <= 9; p1++)
+                        {
+                            WriteLine($"{p1} * {p2} = {p1 * p2}");
+                        }
+                        WriteLine("--------------------");
+                    }
+                    loop = false;
+                    break;
+                case "/":
+                    for (float p2 = 1; p2 <= 9; p2++)
+                    {
+                        for (float p1 = 1; p1 <= 9; p1++)
+                        {
+                            WriteLine($"{p1} / {p2} = {p1 / p2}");
+                        }
+                        WriteLine("--------------------");
+                    }
+                    loop = false;
+                    break;
+                case "0":
+                    loop = false;
+                    break;
+                default:
+                    WriteLine("Você digitou um operando inválido.");
+                    break;
             }
-            loop = false;
-            break;
-        case "-":
-            for (float p2 = 1; p2 <= 9; p2++)
-            {
-                for (float p1 = 1; p1 <= 9; p1++)
-                {
-                    WriteLine($"{p1} - {p2} = {p1 - p2}");
-                }
-                WriteLine("--------------------");
-            }
-            loop = false;
-            break;
-        case "*":
-            for (float p2 = 1; p2 <= 9; p2++)
-            {
-                for (float p1 = 1; p1 <= 9; p1++)
-                {
-                    WriteLine($"{p1} * {p2} = {p1 * p2}");
-                }
-                WriteLine("--------------------");
-            }
-            loop = false;
-            break;
-        case "/":
-            for (float p2 = 1; p2 <= 9; p2++)
-            {
-                for (float p1 = 1; p1 <= 9; p1++)
-                {
-                    WriteLine($"{p1} / {p2} = {p1 / p2}");
-                }
-                WriteLine("--------------------");
-            }
-            loop = false;
-            break;
-        case "0":
-            loop = false;
-            break;
-        default:
-            WriteLine("Você digitou um operando inválido.");
-            break;
+        }
     }
 }
