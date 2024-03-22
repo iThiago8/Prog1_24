cbf2f85b38bdbf6f065db3bb6f205353db4ba3ff
iThiago8 <thiago.pedro.padilha899@gmail.com>
2024-03-22 19:00:51 -0300

    Inicio

---
+++ Atividades Casa/Atividade240318/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Atividade240318.models;
+
+class Program 
+{
+    static void Main()
+    {
+        Cliente c1 = new Cliente();
+
+        c1.ClienteID = 1;
+        c1.Nome = "Thiago Pedro Padilha";
+        c1.Email = "thiago.pedro.padilha@gmail.com";
+        c1.DataNascimento = new DateTime();
+        c1.Sexo = "Masculino";
+        c1.CEP = "89564-346";
+        c1.Rua = "Araçatuba";
+        c1.Numero = 202;
+        c1.Bairro = "Sesi";
+        c1.Cidade = "Videira";
+        c1.Estado = "Santa Catarina";
+        c1.País = "Brasil";
+    }
+}
