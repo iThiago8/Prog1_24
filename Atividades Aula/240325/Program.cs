eacd368556e6067301599fc844f2c574a72101c1
Thiago Pedro Padilha <411730@unoesc.lan>
2024-03-25 22:08:13 -0300

    Pilares POO e Modificadores

---
+++ Atividades Aula/240325/Program.cs
@@ -0,0 +1,16 @@
+﻿//Primeira Clinica
+using _240325.models;
+
+Clinica c1 = new Clinica();
+Clinica.InstanceCount = 1;
+c1.ObjectCount = 1;
+
+//Segunda Clinica
+Clinica c2 = new Clinica();
+Clinica.InstanceCount++;
+c2.ObjectCount = 10;
+
+Console.WriteLine($"Valor c1: {c1.ObjectCount}");
+Console.WriteLine($"Instance Count: {Clinica.InstanceCount}");
+
+Console.WriteLine($"Valor c2: {c2.ObjectCount}");
